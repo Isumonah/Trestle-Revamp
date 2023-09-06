cbc44fb3569c103c32d72e1f2521317c36f903f4
Anna Isumonah <maisumonah@gmail.com>
2023-09-06 09:20:32 +0100

    changes

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -8,13 +8,13 @@ export default function Filter(){
                     <option value="fresh-finds">Fresh Finds</option>
                     <option value="Paycycle">Paycycle</option>
                 </select>
-                <select name="sector" id="sector" className="my-3 mr-4 lg:px-3 w-[110px] sm:w-auto">
+                <select name="sector" id="sector" className="my-3 mr-4 py-2 lg:px-3 sm:w-auto">
                     <option value="Sector">Sector</option>
                     <option value="fashion">Fashion</option>
                     <option value="agriculture">Agriculture</option>
                     <option value="recylcling">Recycling</option>
                 </select>
-                <select name="funding-stage" id="funding-stage" className="my-3 mr-4 py-2 lg:px-3 xl:px-0 w-[110px] sm:w-auto">
+                <select name="funding-stage" id="funding-stage" className="my-3 mr-4 py-2 lg:px-3 sm:w-auto">
                     <option value="Funding Stage">Funding Stage</option>
                     <option value="Initial-Stage">Initial Stage</option>
                     <option value="Intermediate">Intermediate</option>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -33,7 +33,7 @@ export default function Nav(){
         </div>
         
         <div className="bg-white">
-            <nav className={`nav-pages ${navState ? "h-[100vh] right-0 w-8/12 bg-[#4A6DA7] text-[white] " : "h-0 right-[-100%]"}  sm:w-11/12 lg:w-10/12 mx-auto absolute sm:relative transition-all overflow-hidden sm:py-2 sm:h-auto sm:left-0`}>
+            <nav className={`nav-pages ${navState ? "h-[100vh] right-0 w-8/12 bg-[#4A6DA7] sm:bg-white text-[white] " : "h-0 right-[-100%]"}  sm:w-11/12 lg:w-10/12 mx-auto absolute sm:relative transition-all overflow-hidden sm:py-2 sm:h-auto sm:left-0`}>
             <div className="flex justify-end mr-4 mt-2">
                 <FaTimes className="h-4 sm:hidden hover:text-[#D7AEFB]" onClick={() => {
                     setNavState(false)
@@ -41,25 +41,25 @@ export default function Nav(){
             </div>
                 <ul className={`flex flex-col space-y-4 text-white sm:space-y-0 pl-4 pr-2 sm:pl-0 sm:pr-0 sm:pt-0 h-1/2 sm:h-auto sm:flex-row sm:justify-between sm:relative w-full text-[15px] sm:text-[16px] md:text-[20px] lg:text-[24px] xl:text-[27px] text-gray-700 font-bold sm:text-black sm:text-[#908989] uppercase sm:normal-case font-[Roboto]`}>
                     <li>
-                        <a href="/about" className={`about-us nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/about' ? 'text-[#FFB6C1] sm:text-[#4A6DA7]' : ''}`}>About Us</a>
+                        <a href="/about" className={`about-us nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/about' ? 'bg-white text-[#0b113d] sm:text-[#4A6DA7] px-2 py-1 sm:p-0 rounded' : ''}`}>About Us</a>
                     </li>
                     <li>
-                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/invest' ? 'text-[#FFB6C1] sm:text-[#4A6DA7]' : ''}`} href="/invest ">Invest</a>
+                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/invest' ? 'bg-white text-[#0b113d] sm:text-[#4A6DA7] px-2 py-1 sm:p-0 rounded' : ''}`} href="/invest ">Invest</a>
                     </li>
                     <li>
-                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/find-investor' ? 'text-[#FFB6C1] sm:text-[#4A6DA7]' : ''}`} href="/find-investor">Find An Investor</a>
+                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/find-investor' ? 'bg-white text-[#0b113d] sm:text-[#4A6DA7] px-2 py-1 sm:p-0 rounded' : ''}`} href="/find-investor">Find An Investor</a>
                     </li>
                     <li>
-                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/events' ? 'text-[#FFB6C1] sm:text-[#4A6DA7]' : ''}`} href="/events">Events</a>
+                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/events' ? 'bg-white text-[#0b113d] sm:text-[#4A6DA7] px-2 py-1 sm:p-0 rounded' : ''}`} href="/events">Events</a>
                     </li>
                     <li>
-                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/success-stories' ? 'text-[#FFB6C1] sm:text-[#4A6DA7]' : ''}`} href="/success-stories">Success Stories</a>
+                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/success-stories' ? 'bg-white text-[#0b113d] sm:text-[#4A6DA7] px-2 py-1 sm:p-0 rounded' : ''}`} href="/success-stories">Success Stories</a>
                     </li>
                     <li className="sm:hidden">
-                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/sign-in' ? 'text-[#FFB6C1] sm:text-[#4A6DA7]' : ''}`} href="/sign-in">Log in</a>
+                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/sign-in' ? 'bg-white text-[#0b113d] sm:text-[#4A6DA7] px-2 py-1 sm:p-0 rounded' : ''}`} href="/sign-in">Log in</a>
                     </li>
                     <li className="sm:hidden">
-                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/sign-up' ? 'text-[#FFB6C1] sm:text-[#4A6DA7]' : ''}`} href="/sign-up
+                        <a className={`nav-pages-link hover:text-[#D7AEFB] ${location.pathname === '/sign-up' ? 'bg-white text-[#0b113d] sm:text-[#4A6DA7] px-2 py-1 sm:p-0 rounded' : ''}`} href="/sign-up
                         ">Sign up</a>
                     </li>
                 </ul>
